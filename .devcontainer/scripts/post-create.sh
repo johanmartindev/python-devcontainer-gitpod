646e07ca3da1c37b81d0d5c42f1e21d2f79b20fa
Johan Martin <jnm+johanmartindev@catenare.com>
2024-03-10 21:26:21 +0000

    Update post-create

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -14,3 +14,4 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 pip install -r /workspace/requirements.txt
+pre-commit
