a6414677ff523fc3fb177be57c63f8a3b53181dc
Johan Martin <jnm+johanmartindev@catenare.com>
2024-03-10 20:58:41 +0000

    Add pip install to gitpod.io

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -2,6 +2,8 @@
 set -xe
 git config --global pull.rebase true
 git config --global --add safe.directory /workspace
+git config --global user.name "Johan Martin"
+git config --global user.email "jnm+johanmartindev@catenare.com"
 git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
 if git config --global gpg.ssh.program; then
